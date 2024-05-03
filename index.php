79b98f9a1405ce6a277e934651c3c60a304bd0a2
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-05-03 15:35:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="server.php">
+        <div>
+            <label for="">Inserisci un paragrafo:</label>
+            <textarea name="paragraph" placeholder="Il tuo paragrafo"></textarea>
+        </div>
+        <div>
+            <label for="" name="word">Inserisci una parola cattiva</label>
+            <input type="text" name="word">
+        </div>
+        <button>Invia</button>
+    </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,17 @@
+<?php
+// var_dump($_GET);
+
+$paragraph = $_GET["paragraph"];
+
+$word = $_GET["word"];
+
+$new_paragraph = str_replace( 'stupidino', '***', $paragraph);
+?>
+
+<h1><?php echo $word ?></h1>
+<h2>Paragrafo non filtrato:</h2>
+<p><?php echo $paragraph ?></p>
+<h4>Numero caratteri del paragrafo: <?php echo strlen($paragraph) ?></h4>
+<h2>Paragrafo filtrato:</h2>
+<p> <?php echo $new_paragraph ?></p>
+<h4>Numero caratteri del paragrafo: <?php echo strlen($new_paragraph) ?></h4>
